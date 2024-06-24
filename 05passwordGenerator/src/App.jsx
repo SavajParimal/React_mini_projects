24c257937d4fca715be1eeac69934074ce6e3557
savaj parimal <savajparimal001@gmail.com>
2024-06-24 10:25:54 +0530

    mini project

--- 05passwordGenerator/src/App.jsx
+++ 05passwordGenerator/src/App.jsx
@@ -1,8 +1,12 @@
+
+import './App.css'
+
 function App() {
-  
 
   return (
-    <h1>react with vite</h1>
+    <>
+     
+    </>
   )
 }
 
--- Learn_React/01basicreact/src/App.js
+++
@@ -1,7 +0,0 @@
-function App() {
-  return (
-    <h1>only react</h1>
-  );
-}
-
-export default App;
